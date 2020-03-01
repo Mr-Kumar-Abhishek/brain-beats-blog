80c56f22c31870f037bd088d57f07582b0c61e14
Abhishek Kumar <kumar.abhishek@techie.com>
2020-03-01 12:50:46 +0530

    update service worker path

--- js/serviceLoader.js
+++ js/serviceLoader.js
@@ -2,7 +2,7 @@ window.addEventListener('load', ()=>{
 
   if('serviceWorker' in navigator){
     try {
-      navigator.serviceWorker.register('serviceWorker.js');
+      navigator.serviceWorker.register('https://mr-kumar.tk/js/serviceWorker.js');
       console.log("Service Worker Registered");
     } catch (error) {
       console.log("Service Worker Registration Failed");
