8cd475541439a2204d821146ada6f42789cbbd84
Abhishek Kumar <kumar.abhishek@techie.com>
2020-03-01 12:58:50 +0530

    fix bug in serviceLoader URL

--- js/serviceLoader.js
+++ js/serviceLoader.js
@@ -2,7 +2,7 @@ window.addEventListener('load', ()=>{
 
   if('serviceWorker' in navigator){
     try {
-      navigator.serviceWorker.register('https://mr-kumar.tk/js/serviceWorker.js');
+      navigator.serviceWorker.register('https://mr-kumar.tk/serviceWorker.js');
       console.log("Service Worker Registered");
     } catch (error) {
       console.log("Service Worker Registration Failed");
