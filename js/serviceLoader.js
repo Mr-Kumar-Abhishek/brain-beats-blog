128c725b11518dc141f181b0277d3dedd06e5352
Abhishek Kumar <mr.kumar.abhishek@email.com>
2020-05-29 11:26:54 +0530

    update service worker site

--- js/serviceLoader.js
+++ js/serviceLoader.js
@@ -2,7 +2,7 @@ window.addEventListener('load', ()=>{
 
   if('serviceWorker' in navigator){
     try {
-      navigator.serviceWorker.register('https://mr-kumar.tk/serviceWorker.js');
+      navigator.serviceWorker.register('https://abhishek-kumar.tk/serviceWorker.js');
       console.log("Service Worker Registered");
     } catch (error) {
       console.log("Service Worker Registration Failed");
